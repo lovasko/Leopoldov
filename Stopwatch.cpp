378e53db330212ec9d913aab640e8e4c79fd25f7
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-06 21:08:22 +0200

    Made the code compile on OSX

--- Stopwatch.cpp
+++ Stopwatch.cpp
@@ -1,14 +1,21 @@
-
-
 #include "Stopwatch.h"
+
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
 
 
 ticks_t now()
 {
-#ifdef _WIN32
-	LARGE_INTEGER ticks;
-	QueryPerformanceCounter(&ticks);
-	return (ticks_t)ticks.QuadPart;
+#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+	clock_serv_t cclock;
+	mach_timespec_t mts;
+	host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+	clock_get_time(cclock, &mts);
+	mach_port_deallocate(mach_task_self(), cclock);
+
+	return (ticks_t)mts.tv_sec * 1000000000UL + (ticks_t)mts.tv_nsec;
 #else
 	struct timespec ts;
 	clock_gettime(CLOCK_REALTIME, &ts);
