11c1f3856bd12b72e16b10753ed0f0289ee03629
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-06 23:30:31 +0200

    Working now with 35% of the serial version

--- du3sch.hpp
+++ du3sch.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <string>
+#include <future>
 #include <thread>
 #include <mutex>
+#include <vector>
+#include <iostream>
 #include <condition_variable>
 
 template <typename T, typename TASK>
@@ -11,13 +14,16 @@ class wrapper
 	public:
 		bool ready;
 		TASK task;
-		std::promise<T> prom;
+		std::promise<T> *prom;
 
-		wrapper (TASK&& _task)
+		wrapper (TASK _task)
 		{
 			ready = false;
 			task = std::move(_task);
+			prom = new std::promise<T>();
 		}
+
+		~wrapper() {}
 };
 
 template<typename T, typename TASK>
@@ -28,7 +34,7 @@ class Scheduler
 		std::condition_variable cv;
 		size_t task_count;
 		std::atomic<unsigned int> train;
-		std::vector<TASK> tasks;
+		std::vector<wrapper<T, TASK>> tasks;
 
 	public:
 		Scheduler(std::size_t core_count)
@@ -47,21 +53,28 @@ class Scheduler
 
 		void worker ()
 		{
-			std::unique_lock<std::mutex>(m);
-			while (! (train < task_count) 
-				cv.wait(lck);	
-			unsigned int local_train_copy = train;
-			train++;
-			
-			tasks[local_train_copy].prom.set_value(tasks[local_train_copy].task());		
-			tasks[local_Train_copy].ready = true;
+			while (1)
+			{
+				std::unique_lock<std::mutex> lck(m);
+				while (train >= task_count)
+					cv.wait(lck);	
+
+				unsigned int local_train_copy = train;
+				train++;
+				lck.unlock();
+				
+				tasks[local_train_copy].prom->set_value(tasks[local_train_copy].task());		
+				tasks[local_train_copy].ready = true;
+
+				std::this_thread::yield();
+			}
 		}
 
 		std::size_t add_task(TASK&& task)
 		{
-			std::unique_lock<std::mutex>(m);
+			std::unique_lock<std::mutex> lck(m);
 
-			wrapper w(task);
+			wrapper<T, TASK> w(task);
 			tasks.push_back(w);
 
 			task_count++;
@@ -77,7 +90,7 @@ class Scheduler
 
 		T get_task_result(std::size_t index)
 		{
-			return tasks[index].prom.get_future().get();	
+			return tasks[index].prom->get_future().get();	
 		}
 };
 
