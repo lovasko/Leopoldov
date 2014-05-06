4412b68ec774c2a2267096241be3bd3f59565e6b
Daniel Lovasko <daniel.lovasko@gmail.com>
2014-05-06 22:24:43 +0200

    WIP

--- du3sch.hpp
+++ du3sch.hpp
@@ -1,32 +1,62 @@
 #pragma once
 
 #include <string>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
 template<typename T, typename TASK>
 class Scheduler
 {
-public:
-	Scheduler(std::size_t core_count)
-	{
-	}
+	private:
+		std::mutex m;
+		std::condition_variable cv;
+		size_t task_count;
+		std::atomic<unsigned int> train;
+		std::vector<TASK> tasks;
 
-	~Scheduler()
-	{
-	}
+	public:
+		Scheduler(std::size_t core_count)
+		{
+			task_count = 0;
+			train = 0;
 
-	std::size_t add_task(TASK&& task)
-	{
-		return 0;
-	}
+		}
 
-	bool is_task_ready(std::size_t index)
-	{
-		return false;
-	}
+		~Scheduler()
+		{
+		}
 
-	T get_task_result(std::size_t index)
-	{
-		return T();
-	}
+		void worker ()
+		{
+			if (train < task_count)
+			{
+				// make the task with std::async but deferred.	
+			}
+			else
+			{
+				// wait on condition variable		
+			}
+		}
+
+		std::size_t add_task(TASK&& task)
+		{
+			std::lock_guard<std::mutex>(m);
+			tasks.push_back(task);
+			task_count++;
+			cv.notify_one();
+
+			return (task_count-1);
+		}
+
+		bool is_task_ready(std::size_t index)
+		{
+			return tasks[index].ready;
+		}
+
+		T get_task_result(std::size_t index)
+		{
+			return tasks[index].fut.get();	
+		}
 };
 
